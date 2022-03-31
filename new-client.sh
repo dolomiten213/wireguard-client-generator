29361e71cddebe20913683a8636517c70b4632fd
dolomiten <56438618+dolomiten213@users.noreply.github.com>
2022-03-31 23:37:58 +0300

    Update new-client.sh

--- new-client.sh
+++ new-client.sh
@@ -1,31 +1,26 @@
-if  [ ! -d clients ]; then mkdir clients
+if  [ ! -d /etc/wireguard/clients ]; then mkdir /etc/wireguard/clients
 fi
 read -p "Client name: " client
 read -p "Device name: " device
 
-cd clients
-if [ ! -d $client ]; then mkdir $client
+if [ ! -d $client ]; then mkdir /etc/wireguard/$client
 fi
-cd $client
-if [ ! -d $device ]; then mkdir $device
+
+if [ ! -d $device ]; then mkdir /etc/wireguard/$client/$device
         else
                 echo already have this client with this device
                 exit
 fi
-cd $device
-wg genkey | tee private-key.txt | wg pubkey > public-key.txt
-cd ..
-cd ..
-cd ..
+wg genkey | tee /etc/wireguard/$client/$device/private-key.txt | wg pubkey > /etc/wireguard/$client/$device/public-key.txt
 
-sprk=`cat server/private-key.txt`
-spuk=`cat server/public-key.txt`
-cprk=`cat clients/$client/$device/private-key.txt`
-cpuk=`cat clients/$client/$device/public-key.txt`
+sprk=`cat /etc/wireguard/server/private-key.txt`
+spuk=`cat /etc/wireguard/server/public-key.txt`
+cprk=`cat /etc/wireguard/clients/$client/$device/private-key.txt`
+cpuk=`cat /etc/wireguard/clients/$client/$device/public-key.txt`
 
-lip=$((`cat server/last-used-ip.txt` + 1))
-dns=`cat server/dns.txt`
-socket=`cat server/socket.txt`
+lip=$((`cat /etc/wireguard/server/last-used-ip.txt` + 1))
+dns=`cat /etc/wireguard/server/dns.txt`
+socket=`cat /etc/wireguard/server/socket.txt`
 
 echo "
 # ===== auto-generated for $client $device =====
@@ -34,7 +29,7 @@ PublicKey = $cpuk
 AllowedIPs = 10.0.0.$lip/32
 PersistentkeepAlive = 20
 
-" >> wg0.conf
+" >> /etc/wireguard/wg0.conf
 
 echo "[Interface]
 PrivateKey = $cprk
@@ -45,14 +40,14 @@ DNS = $dns
 PublicKey = $spuk
 Endpoint = $socket
 AllowedIPs = 0.0.0.0/0
-PersistentKeepalive = 20" > clients/$client/$device/$client-$device.conf
+PersistentKeepalive = 20" > /etc/wireguard/clients/$client/$device/$client-$device.conf
 
-echo $lip > server/last-used-ip.txt
+echo $lip > /etc/wireguard/server/last-used-ip.txt
 
 systemctl restart wg-quick@wg0
 #systemctl status wg-quick@wg0
 
-qrencode -t ansiutf8 < clients/$client/$device/$client-$device.conf
-cat clients/$client/$device/$client-$device.conf
+qrencode -t ansiutf8 < /etc/wireguard/clients/$client/$device/$client-$device.conf
+cat /etc/wireguard/clients/$client/$device/$client-$device.conf
 
 #rm -rf clients/
